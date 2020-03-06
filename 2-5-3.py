463bfd39776ebeaff8503f3fe4438e382978d574
savva-kotov <arlekin0011@gmail.com>
2020-03-06 16:50:20 +0300

    Update 2-5-3.py

--- 2-5-3.py
+++ 2-5-3.py
@@ -1,6 +1,6 @@
 """
-Напишите программу, которая принимает на вход список чисел в одной строке и выводит на экран в одну строку значения, которые повторяются в нём более одного раза.
-Для решения задачи может пригодиться метод sort списка.
+Напишите программу, которая принимает на вход список чисел в одной строке и выводит на экран в одну строку значения, 
+которые повторяются в нём более одного раза. Для решения задачи может пригодиться метод sort списка.
 Выводимые числа не должны повторяться, порядок их вывода может быть произвольным.
 
 Sample Input 1:
@@ -22,30 +22,15 @@ Sample Input 4:
 Sample Output 4:
 1 2
 """
-
 s = list(int(i) for i in input().split())
-s.sort()
-n = 0
-l = len(s) - 1
-t = []
-if len(s) == 1:
-    print()
-else:
-    for i in range(l):
-        if s[i] == s[i + 1]:
-            n += 1
-            continue
-        elif i == 0 and s[i] != s[i + 1]:
-            continue
-        elif len(s) == 1:
-            break
-        elif i != 0 and s[i] != s[i - 1] and s[i] != s[i + 1]:
-            continue
-        else:
-            t.append(s[i])
-            n = 1
-    if s[l] == s[l - 1]:
-        t.append(s[l])
-    t.sort()
-    for i in range(len(t)):
-        print(t[i], end=" ")
+d = {}
+for i in s:
+    if i not in d:
+        d[i] = 1
+    else:
+        d[i] += 1
+res = []
+for i in d.keys():
+    if d[i] >= 2:
+        res.append(i)
+print(*res)
